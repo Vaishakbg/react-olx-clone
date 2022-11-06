326279775eda4d409d11ad97b8dda6fc9bb0d3f8
Vaishakbg <vaishakbg@gmail.com>
2022-11-06 14:31:22 +0530

    source file from tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Home />
     </div>
   );
 }
