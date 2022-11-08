f011e01e0ab64e6ece2c9cc364dc6c14d6481a62
Vaishakbg <vaishakbg@gmail.com>
2022-11-08 18:59:12 +0530

    completed add product

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,24 +1,27 @@
-import React, { Fragment, useContext, useState } from 'react';
-import './Create.css';
-import Header from '../Header/Header';
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import { addDoc, collection } from 'firebase/firestore/lite';
-import { AuthContext, FirebaseContext } from '../../store/FirebaseContext';
+import React, { Fragment, useContext, useState } from "react";
+import "./Create.css";
+import Header from "../Header/Header";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { addDoc, collection } from "firebase/firestore/lite";
+import { AuthContext, FirebaseContext } from "../../store/FirebaseContext";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-  const [name, setName] = useState('')
-  const [category, setCategory] = useState('')
-  const [price, setPrice] = useState('')
-  const [image, setImage] = useState(null)
-  const {firebase, storage} = useContext(FirebaseContext)
-  const {user} = useContext(AuthContext)
-  const date = new Date()
+  const { firebase, storage } = useContext(FirebaseContext);
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const date = new Date();
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
 
-  const handleSubmit = ()=>{
+  const handleSubmit = () => {
     const storageRef = ref(storage, `/images/${image.name}`);
     const uploadTask = uploadBytesResumable(storageRef, image);
-    uploadTask.on("state_changed",
-      
+    uploadTask.on(
+      "state_changed",
+
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           console.log(downloadURL);
@@ -29,62 +32,77 @@ const Create = () => {
             price,
             url: downloadURL,
             userId: user.uid,
-            createdAt: date.toDateString()
-          }
+            createdAt: date.toDateString(),
+          };
           console.log(user, data);
           addDoc(collection(firebase, "products"), data).then(() => {
-            console.log('product added!');
-          })
+            console.log("product added!");
+            navigate("/");
+          });
         });
       },
       (error) => {
         console.log(error);
       }
     );
-  }
+  };
 
   return (
     <Fragment>
       <Header />
       <div>
         <div className="centerDiv">
-            <label htmlFor="fname">Name</label>
-            <br />
-            <input
-              className="input"
-              type="text"
-              id="fname"
-              name="Name"
-              value={name}
-              onChange={(e)=> setName(e.target.value)}
-            />
-            <br />
-            <label htmlFor="category">Category</label>
-            <br />
-            <input
-              className="input"
-              type="text"
-              id="category"
-              name="category"
-              value={category}
-              onChange={(e)=> setCategory(e.target.value)}
-            />
-            <br />
-            <label htmlFor="price">Price</label>
-            <br />
-            <input className="input" type="number" id="price" name="Price"
-            value={price}
-            onChange={(e)=> setPrice(e.target.value)}
-            />
-            <br />
+          <label htmlFor="fname">Name</label>
           <br />
-          <img alt="Posts" width="200px" height="200px" src={image ? URL.createObjectURL(image):''}></img>
-            <br />
-            <input type="file" onChange={(e)=> {
-              setImage(e.target.files[0])
-            }}/>
-            <br />
-            <button className="uploadBtn" onClick={handleSubmit}>upload and Submit</button>
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            name="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <label htmlFor="category">Category</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            id="category"
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <br />
+          <label htmlFor="price">Price</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            id="price"
+            name="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <br />
+          <br />
+          <img
+            alt="Posts"
+            width="200px"
+            height="200px"
+            src={image ? URL.createObjectURL(image) : ""}
+          ></img>
+          <br />
+          <input
+            type="file"
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+          />
+          <br />
+          <button className="uploadBtn" onClick={handleSubmit}>
+            upload and Submit
+          </button>
         </div>
       </div>
     </Fragment>
